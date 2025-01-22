6240eb9e0eae9a1c504f611965c05e01a95f7e06
MayurPillai121 <mayurpillai121@gmail.com>
2025-01-22 18:27:40 +0530

    Simplify replit.nix configuration

--- replit.nix
+++ replit.nix
@@ -1,16 +1,9 @@
 { pkgs }: {
   deps = [
-    pkgs.python39Full
-    pkgs.gcc
-    pkgs.nodejs
-    pkgs.poetry
+    pkgs.python3
   ];
   env = {
-    PYTHONBIN = "${pkgs.python39Full}/bin/python3.9";
+    PYTHONBIN = "${pkgs.python3}/bin/python3";
     LANG = "en_US.UTF-8";
-    PIP_ROOT_USER_ACTION = "ignore";
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.python39Full
-    ];
   };
 }
