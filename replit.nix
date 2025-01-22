29a8c716370ee98afe1bbfd42b04a7f7b4862aeb
MayurPillai121 <mayurpillai121@gmail.com>
2025-01-22 18:19:53 +0530

    Update Replit configuration with proper run command and dependencies

--- replit.nix
+++ replit.nix
@@ -1,10 +1,15 @@
 { pkgs }: {
   deps = [
-    pkgs.python3
-    pkgs.python3Packages.pip
+    pkgs.python39
+    pkgs.python39Packages.pip
+    pkgs.python39Packages.flask
+    pkgs.python39Packages.numpy
+    pkgs.python39Packages.pillow
+    pkgs.python39Packages.requests
   ];
   env = {
-    PYTHONBIN = "${pkgs.python3}/bin/python";
+    PYTHONBIN = "${pkgs.python39}/bin/python3.9";
     LANG = "en_US.UTF-8";
+    PIP_ROOT_USER_ACTION = "ignore";
   };
 }
