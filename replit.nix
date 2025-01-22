75941b01e8471fa10390616779d8b062e440f4f5
MayurPillai121 <mayurpillai121@gmail.com>
2025-01-22 20:36:44 +0530

    Update dependencies for Python 3.12 compatibility

--- replit.nix
+++ replit.nix
@@ -1,16 +1,16 @@
 { pkgs }: {
   deps = [
-    pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
+    pkgs.python312
+    pkgs.replitPackages.prybar-python312
     pkgs.replitPackages.stderred
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.python310Full
+      pkgs.python312
     ];
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    PYTHONBIN = "${pkgs.python312}/bin/python3.12";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python312}/bin/prybar-python312";
   };
 }
