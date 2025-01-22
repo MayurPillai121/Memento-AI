b2c8911676f3be1c52c4847261e4190b6781d215
MayurPillai121 <mayurpillai121@gmail.com>
2025-01-22 21:10:46 +0530

    Update Replit configuration to fix pip and Python path issues

--- replit.nix
+++ replit.nix
@@ -1,6 +1,9 @@
 { pkgs }: {
   deps = [
     pkgs.python310Full
+    pkgs.python310Packages.pip
+    pkgs.python310Packages.setuptools
+    pkgs.python310Packages.wheel
     pkgs.replitPackages.prybar-python310
     pkgs.replitPackages.stderred
   ];
@@ -12,5 +15,7 @@
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
     PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+    PIP_DISABLE_PIP_VERSION_CHECK = "1";
+    PYTHONPATH = "${pkgs.python310Packages.pip}/lib/python3.10/site-packages";
   };
 }
