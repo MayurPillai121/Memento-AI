2a4e92bcceec21ecd6d526f7334c75ce702c6f21
MayurPillai121 <mayurpillai121@gmail.com>
2025-01-22 21:12:33 +0530

    Simplify Replit configuration and fix Python environment

--- replit.nix
+++ replit.nix
@@ -1,21 +1,20 @@
 { pkgs }: {
   deps = [
-    pkgs.python310Full
-    pkgs.python310Packages.pip
-    pkgs.python310Packages.setuptools
-    pkgs.python310Packages.wheel
+    pkgs.python310
+    (pkgs.python310.withPackages (ps: [
+      ps.pip
+      ps.setuptools
+      ps.wheel
+      ps.flask
+      ps.gunicorn
+    ]))
     pkgs.replitPackages.prybar-python310
     pkgs.replitPackages.stderred
   ];
   env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.python310Full
-    ];
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    PYTHONBIN = "${pkgs.python310}/bin/python3.10";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
     PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
-    PIP_DISABLE_PIP_VERSION_CHECK = "1";
-    PYTHONPATH = "${pkgs.python310Packages.pip}/lib/python3.10/site-packages";
   };
 }
