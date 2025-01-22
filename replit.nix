d8b00417ef3703f6b53b11763b79cf1b138478db
MayurPillai121 <mayurpillai121@gmail.com>
2025-01-22 18:08:43 +0530

    Fix: Update replit.nix to use python39Full and remove prybar references

--- replit.nix
+++ replit.nix
@@ -1,18 +1,16 @@
 { pkgs }: {
   deps = [
-    pkgs.python39
+    pkgs.python39Full
     pkgs.python39Packages.pip
     pkgs.python39Packages.flask
     pkgs.replitPackages.stderred
-    pkgs.replitPackages.prybar-python39
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.python39
+      pkgs.python39Full
     ];
-    PYTHONBIN = "${pkgs.python39}/bin/python3.9";
+    PYTHONBIN = "${pkgs.python39Full}/bin/python3.9";
     LANG = "en_US.UTF-8";
     STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python39}/bin/prybar-python39";
   };
 }
